627913e229b0fc533d636bfee01eff9fa88fde2e
w3cj <cj@null.computer>
2025-03-08 15:42:20 -0700

    Updated the readme file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hex Guesser
+
+*A game that produces a color image, with a prompt. You have to deduce what the color in hex or rgb value what the color is*
+
+## General Game Requirements:
+- This game will be created for the browser
+  - Optimization for browser should be paramount.
+  - Take advantage of browser tricks to keep the game quick even on limited computers.
+- Create an image with a specified color. *Can't just use a div and color it as we are looking for the general and it would be too easy to cheat*
+  - This image should have no indication of what the initial color is eg. **hash the file**.
+  - The image should be small so that we can take advantage of speed.
+
+### This game is a work in progress.
--- img-generator.py
+++ img-generator.py
@@ -2,13 +2,8 @@ from PIL import Image
 import hashlib
 from random import randint
 
-def generate_1px_image(hex_color, filename="color_pixel.png"):
-    # Create a 1x1 image with the specified color
-    img = Image.new("RGB", (1, 1), hex_color)
-    
-    # Save the image
-    img.save(filename)
-    print(f"1px image saved as {filename} with color {hex_color}")
+# Generate a random color 1 pixel image.
+# Given nothing generate an image that is stored in an images folder within this parent folder
 
 def generate_random_1px_image():
     color = pick_random_color()
@@ -16,18 +11,29 @@ def generate_random_1px_image():
     name = generate_name(color)
     generate_1px_image(hexval, "images/" + name)
 
+
+# Generate a 1 pixel image with a specified hex color
+# Given a hex_color and a file name generate a 1 pixel image and store the image in the specified filename
+# generate_1px_image("#000000", img_02d74341075fdbda814e5f46c66c9a7d.png) -> image with the file name
+
+def generate_1px_image(hex_color, filename="color_pixel.png"):
+    img = Image.new("RGB", (1, 1), hex_color)
+    img.save(filename)
+
 # Generate Random RGB Color dictionary
 # Given none return a dictionary with integer values for "r", "g", "b" that are between 0 and 255
 # pick_random_color() -> {"r": 0, "g": 0, "b": 0}
+
 def pick_random_color():
     r = randint(0, 256)
     g = randint(0, 256)
     b = randint(0, 256)
     return {"r": r, "g": g, "b": b}
 
+# Genrate a Hex String given a color dictionary
 # given a color dictionary containing integer rgb values provide a hex string back with # sign at the front
 # generate_hex_str({"r": 0, "g": 0, "b", 0}) -> #000000
-#
+
 def generate_hex_str(color):
     rstr = '{:02x}'.format(color['r'])
     gstr = '{:02x}'.format(color['g'])
@@ -38,9 +44,8 @@ def generate_hex_str(color):
 # Generate a replicatable name that isn't associated with the hex code color
 # Given a color dictionary with integer values for "r", "g", "b" that are between 0 and 255
 # return a name that is hashed and duplicatable
-# generate_name({"r": 0, "g": 0, "b":0 }) -> 
+# generate_name({"r": 0, "g": 0, "b":0 }) -> img_02d74341075fdbda814e5f46c66c9a7d.png
 
-# generate_name(color(dict)) -> str
 def generate_name(color):
     s = "r"+str(color['r'])+"g"+ str(color['g']) +"b"+ str(color['b'])
     e = s.encode('utf-8')
@@ -48,4 +53,3 @@ def generate_name(color):
     return "img_" + h + ".png"
 
 generate_random_1px_image()
-print(generate_name({"r":0, "g": 0, "b": 0}))
